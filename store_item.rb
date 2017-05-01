42a4bde481f4c53ba476d72c836ea5861fd2fbb8
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-05-01 19:40:23 -0400

    initialize

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+gummy1 = {color: "blue", price: "5", flavour: "ocean", size: "large"}
+gummy2 = {:color => "pink", :price => "2", flavour: "berry", size: "medium"}
+gummy1 = {color: "golden", price: "10", :flavour => "gold", :size => "small"}
+
+
+puts "We have 3 gummy bears for sale today.  We have a #{gummy1[:size]} #{gummy1[:color]} that is #{gummy1[:price]}."
